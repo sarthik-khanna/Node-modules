ebd9d0fb8f1f13749821353f9292b59b0e568130
khanna-sarthik <sarthikkhanna28@gmail.com>
2025-02-15 21:48:41 +0530

    first commit

---
+++ hello.js
@@ -0,0 +1,4 @@
+// console.log("hello node js today i will start u to under stand the concept");
+const math = require('./math');
+// console.log("math value is :",math(2,7));
+console.log("subtract value is:",math.subFn(9,7));
---
+++ math.js
@@ -0,0 +1,11 @@
+function add(a,b){
+    return a+b;
+}
+
+function subtract(x,y){
+    return x-y;
+}
+module.exports={
+    addFn:add,
+    subFn:subtract,
+};
